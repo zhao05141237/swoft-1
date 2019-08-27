ee612bad2ab1e0918e146d70193047908201694b
zhaoqi <zhao361020136@gmail.com>
2019-08-27 10:58:15 +0800

    test exception handler

--- app/Http/Controller/TimerController.php
+++ app/Http/Controller/TimerController.php
@@ -3,11 +3,14 @@
 
 namespace App\Http\Controller;
 
-use App\Model\Entity\User;
+use App\Model\Entity\Users;
 use Exception;
+use ReflectionException;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\Db\Exception\DbException;
 use Swoft\Http\Server\Annotation\Mapping\Controller;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
-use Swoft\Log\Helper\Log;
+use Swoft\Log\Helper\CLog;
 use Swoft\Redis\Redis;
 use Swoft\Stdlib\Helper\JsonHelper;
 use Swoft\Timer;
@@ -29,24 +32,45 @@ class TimerController
      */
     public function after(): array
     {
-        Timer::after(3 * 1000, function (int $timerId) {
-            $user = new User();
-            $user->setAge(mt_rand(1, 100));
-            $user->setUserDesc('desc');
-
-            $user->save();
-            $id = $user->getId();
-
-            Redis::set("$id", $user->toArray());
-            Log::info("用户ID=" . $id . " timerId=" . $timerId);
-            sgo(function () use ($id) {
-                $user = User::find($id)->toArray();
-                Log::info(JsonHelper::encode($user));
-                Redis::del("$id");
-            });
-        });
+        $paramOne = 'aa';
+        $paramTwo = 'bb';
+        Timer::after(3 * 1000, function (int $timerId, $paramOne, $paramTwo) {
+            CLog::info('time_id' . $timerId);
+            CLog::info('param' . $paramOne);
+            CLog::info($paramTwo);
+            $this->addUser($timerId);
+        }, $paramOne, $paramTwo);
 
         return ['after'];
+    }
+
+    /**
+     * @param $timerId
+     * @throws ReflectionException
+     * @throws ContainerException
+     * @throws DbException
+     */
+    private function addUser($timerId)
+    {
+        $rand = mt_rand(1111111, 9999999);
+        $name = 'qi.zhao' . $rand;
+        $email = $name . '@163.com';
+        $password = password_hash($name, PASSWORD_DEFAULT);
+
+        $user = new Users();
+        $user->setName($name);
+        $user->setEmail($email);
+        $user->setPassword($password);
+        $user->save();
+        $id = $user->getId();
+
+        Redis::set("$id", $user->toArray());
+        CLog::info("用户ID=" . $id . " timerId=" . $timerId);
+        sgo(function () use ($id) {
+            $user = Users::find($id)->toArray();
+            CLog::info(JsonHelper::encode($user));
+            Redis::del("$id");
+        });
     }
 
     /**
@@ -58,20 +82,7 @@ class TimerController
     public function tick(): array
     {
         Timer::tick(3 * 1000, function (int $timerId) {
-            $user = new User();
-            $user->setAge(mt_rand(1, 100));
-            $user->setUserDesc('desc');
-
-            $user->save();
-            $id = $user->getId();
-
-            Redis::set("$id", $user->toArray());
-            Log::info("用户ID=" . $id . " timerId=" . $timerId);
-            sgo(function () use ($id) {
-                $user = User::find($id)->toArray();
-                Log::info(JsonHelper::encode($user));
-                Redis::del("$id");
-            });
+            $this->addUser($timerId);
         });
 
         return ['tick'];
